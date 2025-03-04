2b551b682a9eb6683e1e07ab665b1c67a92f70fe
Mohamed Ben Abdelouhab <mohaben-@c2r1p5.1337.ma>
2025-03-04 14:57:19 +0000

    minishell

--- minishell.c
+++ minishell.c
@@ -18,7 +18,7 @@ char	*ft_get_path(char **envp)
 
 void	p_error_cmd(char **cmd, char **paths, int exit_status)
 {
-	write(2, "minishell: ", 7);
+	write(2, "minishell: ", 11);
 	if (cmd[0])
 		write(2, cmd[0], ft_strlen(cmd[0]));
 	else
@@ -57,7 +57,7 @@ void	ft_exec_cmd(char *cmd, char **envp)
 
 void	ft_handle_sigint(int sig)
 {
-	(void)sig;
+ 	(void)sig;
 	write(1, "\nminishell> ", 12);
 }
 
@@ -75,20 +75,33 @@ int main(int ac, char **av, char **envp)
 	(void)ac;
 	(void)av;
 	signal(SIGINT, ft_handle_sigint);
+	signal(SIGQUIT, SIG_IGN);
 	rl_catch_signals = 0;
 	while (1)
 	{
 		cmd = readline("minishell> ");
+		if (cmd && *cmd)
+			add_history(cmd);
 		if (!cmd)
 		{
 			write(1, "exit\n", 5);
+			clear_history();
 			exit(0);
 		}
 		if (!ft_strncmp(cmd, "exit", 5))
 		{
 			free(cmd);
+			clear_history();
 			write(1, "exit\n", 5);
 			exit(0);
+		}
+		if (!ft_strncmp(cmd, "echo $", 6))
+		{
+			if (getenv(cmd + 6))
+				printf("%s\n", getenv(cmd +6));
+			else
+				write(1, "\n", 1);
+			continue ;
 		}
 		if (cmd[0] == '\f' && cmd[1] == '\0')
 		{
@@ -112,4 +125,5 @@ int main(int ac, char **av, char **envp)
 			write(2, "Fork error\n", 11);
 		free(cmd);
 	}
+	return (0);
 }
