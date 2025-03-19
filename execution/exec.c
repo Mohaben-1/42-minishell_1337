186777044a409fad4a56ced7b967c1830110b404
mohaben-1 <mohammedwhb2001@gmail.com>
2025-03-19 16:50:04 +0000

    handle pipe

--- execution/exec.c
+++ execution/exec.c
@@ -6,52 +6,61 @@
 /*   By: mohaben- <mohaben-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/15 11:10:22 by mohaben-          #+#    #+#             */
-/*   Updated: 2025/03/18 16:42:20 by mohaben-         ###   ########.fr       */
+/*   Updated: 2025/03/19 15:49:35 by mohaben-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+void	execute_subshell(t_ast_node *ast, t_exec *exec)
+{
+	int	pid;
+	int	status;
+
+	if (!ast || !ast->child)
+		return ;
+	pid = fork();
+	if (pid == -1)
+	{
+		ft_putstr_fd("minishell: fork: Resource temporarily unavailable\n", 2);
+		return ;
+	}
+	if (pid == 0)
+	{
+		ft_apply_redirect(ast->redirects, exec);
+		execute_ast(ast->child, exec);
+		ft_restore_std_fd(exec);
+		exit(exec->exit_status);
+	}
+	waitpid(pid, &status, 0);
+	if (WIFEXITED(status))
+		exec->exit_status = WEXITSTATUS(status);
+	else
+		exec->exit_status = 1;
+}
+
 void	execute_ast(t_ast_node *ast, t_exec *exec)
 {
 	if (!ast)
 		return ;
+	ft_apply_redirect(ast->redirects, exec);
 	if (ast->type == AST_COMMAND)
 		execute_command(ast, exec);
 	else if (ast->type == AST_PIPE)
 		ft_execute_pipe(ast, exec);
-	// else if (ast->type == AST_AND_AND)
-	// 	execute_and_and(ast, exec);
-	// else if (ast->type == AST_OR_OR)
-	// 	execute_or_or(ast, exec);
-	// else if (ast->type == AST_SUBSHELL)
-	// 	execute_subshell(ast, exec);
+	else if (ast->type == AST_AND_AND)
+	{
+		execute_ast(ast->left, exec);
+		if (exec->exit_status == 0)
+			execute_ast(ast->right, exec);
+	}
+	else if (ast->type == AST_OR_OR)
+	{
+		execute_ast(ast->left, exec);
+		if (exec->exit_status != 0)
+			execute_ast(ast->right, exec);
+	}
+	else if (ast->type == AST_SUBSHELL)
+		execute_subshell(ast, exec);
+	ft_restore_std_fd(exec);
 }
-
-// void execute_command(t_ast_node *node, t_exec *exec);
-// void ft_execute_pipe(t_ast_node *node, t_exec *exec);
-// void execute_and_and(t_ast_node *node, t_exec *exec);
-// void execute_or_or(t_ast_node *node, t_exec *exec);
-// void execute_subshell(t_ast_node *node, t_exec *exec);
-
-// void	ft_exec_cmd(t_ast_node *node, t_exec *exec)
-// {
-// 	if (!node)
-// 		return ;
-// 	if (node->type == AST_COMMAND)
-// 		ft_exec_ve(node, exec);
-// 	else if (node->type == AST_AND_AND)
-// 	{
-// 		ft_exec_ve(node->left, exec);
-// 		if (exec->exit_status == 0)
-// 			ft_exec_ve(node->right, exec);
-// 	}
-// 	else if (node->type == AST_OR_OR)
-// 	{
-// 		ft_exec_ve(node->left, exec);
-// 		if (exec->exit_status != 0)
-// 			ft_exec_ve(node->right, exec);
-// 	}
-// 	else if (node->type == AST_PIPE)
-// 		ft_execute_pipe(node, exec);
-// }
--- utils/errors.c
+++ utils/errors.c
@@ -6,7 +6,7 @@
 /*   By: mohaben- <mohaben-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 13:21:07 by mohaben-          #+#    #+#             */
-/*   Updated: 2025/03/18 16:28:44 by mohaben-         ###   ########.fr       */
+/*   Updated: 2025/03/19 11:46:10 by mohaben-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,7 @@ void	ft_error(char *err, int exit_status)
 
 void	ft_error_cmd(char *cmd, char **paths, int exit_status)
 {
-	ft_putstr_fd("minishell", 2);
-	write(2, "minishell: ", 11);
+	ft_putstr_fd("minishell: ", 2);
 	if (cmd)
 		ft_putstr_fd(cmd, 2);
 	else
